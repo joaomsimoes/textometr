a42dac67ffb3e52183874ba251610e0106a6010c
leshkin <alaposhin@gmail.com>
2022-02-03 12:47:13 +0300

    build: frontend 2.0.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/analyze': 'https://textometr.ru', //'http://backend:8888',
+      "/analyze": "https://textometr.ru", // 'http://backend:8888'
     },
   },
   plugins: [
     Components({
-      extensions: ['vue'],
+      extensions: ["vue"],
       include: [/\.vue$/, /\.vue\?vue/],
     }),
     vue(),
   ],
-})
+});
