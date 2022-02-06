880ae221ee550a96dc60351cfca1ab857344fea2
leshkin <alaposhin@gmail.com>
2022-02-06 15:21:41 +0300

    fix: SW

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import { ViteSSG } from 'vite-ssg/single-page'
 import App from './App.vue'
 import './assets/styles.scss'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onRegisterError(error) {}
+})
 
 export const createApp = ViteSSG(App)
