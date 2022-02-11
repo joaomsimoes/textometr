e86fb0c3c8522ef5742459cb69b573748a953782
leshkin <alaposhin@gmail.com>
2022-02-11 16:10:10 +0300

    feat: unregister service worker

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import { ViteSSG } from 'vite-ssg/single-page'
 import App from './App.vue'
 import './assets/styles.scss'
-import { registerSW } from 'virtual:pwa-register'
 
-const updateSW = registerSW({
-  onRegisterError(error) {}
-})
+if (typeof navigator !== 'undefined') {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      registration.unregister()
+    }
+  })
+}
 
 export const createApp = ViteSSG(App)
