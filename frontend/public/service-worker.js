4f24fddb28f4486c181e3c4d4ada5420de830244
leshkin <alaposhin@gmail.com>
2022-02-12 14:01:12 +0300

    fix: remove SW

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,11 +1,3 @@
-if (typeof navigator !== 'undefined') {
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    for (const registration of registrations) {
-      registration.unregister()
-    }
-  })
-}
-
 caches.keys().then(function (names) {
   for (const name of names) caches.delete(name)
 })
