adbcdefb81516570dc0aa68cc3c81198fd0ca7d9
leshkin <alaposhin@gmail.com>
2022-02-12 13:45:18 +0300

    fix: delete service worker immediately

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -9,3 +9,18 @@ if (typeof navigator !== 'undefined') {
 caches.keys().then(function (names) {
   for (const name of names) caches.delete(name)
 })
+
+self.addEventListener('activate', function () {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll()
+    })
+    .then(function (clients) {
+      clients.forEach((client) => {
+        if (client.url && 'navigate' in client) {
+          client.navigate(client.url)
+        }
+      })
+    })
+})
