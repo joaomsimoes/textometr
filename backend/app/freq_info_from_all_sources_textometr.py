674d771b7e7c5ab552d3506fcc02ef516be7d8cb
antoninushka <antonina.laposhina@gmail.com>
2022-02-15 16:38:28 +0300

    frequency analysis page: add omonims and all fields

--- backend/app/freq_info_from_all_sources_textometr.py
+++ backend/app/freq_info_from_all_sources_textometr.py
@@ -1,44 +1,45 @@
-
-
 import pymystem3
 import pandas as pd
 
 m = pymystem3.Mystem(entire_input=False, disambiguation=True)
 
-input_text = "собакой, А, это"
+all_freq_data = pd.read_csv("data/freq_all.csv", quotechar='`')
 
-all_freq_data = pd.read_csv('data/rki_unified_100001.csv', quotechar='`')
+columns = ["lemma", "comment_main", "comment_child", "pos", "cefr", "rnc_ipm", "detcorpus_ipm", "rufola_123_ipm", "det_rki_ipm", "bilingual_ipm","native_ipm"]
 
-columns = ["rank", "lemma", "pos", "all_rki_ipm", "bilingual_ipm","rnc_ipm","detcorpus_ipm"]
-
-list_of_words = m.lemmatize(input_text)
-
-def __find_freq(words):
+def __find_freq(row_words):
+    lem_words = m.lemmatize(row_words)
+    lem_str = ','.join(lem_words)
+    lem_str = lem_str.replace("печение","печенье")
+    lem_str = lem_str.replace("варение", "варенье")
+    lem_str = lem_str.replace("воскресение", "воскресенье")
+    lem_str = lem_str.replace("счастие", "счастье")
+    lem_str = lem_str.replace("несчастие", "несчастье")
+    lem_words_list = lem_str.split(',')
+    words = list(sorted(lem_words_list))
     result = []
+    freq_data_dict = {"input_error": False, "not_in_list": False, "is_omonim": False}
     if len(words) == 0 or len(words) > 3:
-        result.append("Ошибка. Вставьте от 1 до 3 русских слов.")
+        freq_data_dict["input_error"] = True
+        result.append(freq_data_dict)
         return result
     for i in words:
-        freq_data_dict = {}
+        freq_data_dict = {"input_error": False, "not_in_list": False, "is_omonim": False}
         list_of_lemmas = list(all_freq_data['lemma'])
-        if list_of_lemmas.count(i) == 0:
-            freq_data_dict["rank"] = "Слово отсутсвует в частотных списках"
+        count_lemmas = list_of_lemmas.count(i)
+        if count_lemmas == 0:
+            freq_data_dict["not_in_list"] = True
             result.append(freq_data_dict)
-        if list_of_lemmas.count(i) == 1:
+        if count_lemmas == 1:
             for col in columns:
                 freq_data_dict[col] = list(all_freq_data[all_freq_data['lemma'] == i][col])[0]
             result.append(freq_data_dict)
-        if list_of_lemmas.count(i) > 1:
-            freq_data_dict["rank"] = "Омоним"
-            result.append(freq_data_dict)
-            '''
+        if count_lemmas > 1:
+            freq_data_dict["is_omonim"] = True
             omonims = all_freq_data[all_freq_data['lemma'] == i]
-            for ii in omonims:
-                print(ii)
+            for row in omonims.iterrows():
                 for col in columns:
-                    freq_data_dict[col] = list(ii[col])
+                    freq_data_dict[col] = row[1][col]
                 result.append(freq_data_dict)
-                '''
     return result
-__find_freq(list_of_words)
 
